6856eb992c4df0b1665fb371093729130601719b
James <thenetimp@gmail.com>
2018-04-23 01:05:25 +0900

    initialized file

--- app/init.inc.php
+++ app/init.inc.php
@@ -1,11 +1,30 @@
 <?php
 
 define('APP_DIR',__DIR__);
-require_once(APP_DIR . '/lib/includes.inc.php');
-require_once(APP_DIR . '/routes.inc.php');
 
-// Make sure we have our pieces
-list($lang, $locale, $route, $queryString) = array_pad(parseRequestUri(), 4, null);
+// Check if the app dir exists
+if(file_exists(APP_DIR))
+{
+  // Make sure the file cache exists if not then create it.
+  if(!file_exists(APP_DIR . '/events/cache/status.txt'))
+  {
+    touch(APP_DIR . '/events/cache/status.txt');
+  }
+  
+  require_once(APP_DIR . '/lib/includes.inc.php');
+  require_once(APP_DIR . '/routes.inc.php');
 
-// Process the global $route
-processRoute();
+  // Make sure we have our pieces
+  list($lang, $locale, $route, $queryString) = array_pad(parseRequestUri(), 4, null);
+
+  // Process the global $route
+  processRoute();
+
+}
+else
+{
+  echo "No application directory exists";
+}
+
+
+
